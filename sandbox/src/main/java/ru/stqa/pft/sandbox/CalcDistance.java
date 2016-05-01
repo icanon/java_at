91342742645726bbbe02cd8037995e9f9a8e9215
icanon <icanon@yandex.ru>
2016-05-01 21:24:56 +0400

    fix task2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/CalcDistance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/CalcDistance.java
@@ -8,8 +8,9 @@ public class CalcDistance {
 
       Point p1 = new Point(1.0, 2.5);
       Point p2 = new Point(2.1, 4.5);
-      System.out.println("Получаем :" + distance(p1, p2));
+//      System.out.println("Получаем :" + distance(p1, p2));
 
+      System.out.println(p1.distance(2.1, 4.5));
    }
 
    public static double distance(Point pPoint1, Point pPoint2){
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -9,6 +9,7 @@ public class Point {
    public double y;
 
 
+
    public Point(double x, double y) {
       this.x = x;
       this.y = y;
@@ -16,7 +17,14 @@ public class Point {
 
 
 
+   public double distance(double x, double y){
+      return Math.sqrt(Math.pow(x - this.x, 2) + Math.pow(y - this.y,2));
+
+   }
+
+
 }
+
 
 
 
