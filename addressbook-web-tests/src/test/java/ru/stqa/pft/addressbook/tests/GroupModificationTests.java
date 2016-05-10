a25fa68cdda0d1ac98435a0bfe5a03e81834614e
Ivan Sokolov <i.sokolov@cityads.com>
2016-05-10 17:01:09 +0300

    add testGroupDeletion

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 /**
  * Created by i.sokolov on 10.05.2016.
@@ -10,8 +11,12 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification(){
 
-
-        
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("testGGGG1", "testGGGG2", "testGGGGG3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
     }
 
 
