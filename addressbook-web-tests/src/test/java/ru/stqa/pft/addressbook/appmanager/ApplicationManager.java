9db85c65a2f84f98b074fafdb647f35e894f6998
Ivan Sokolov <i.sokolov@cityads.com>
2016-05-11 17:18:49 +0300

    add Webdriver

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -8,17 +12,27 @@ import java.util.concurrent.TimeUnit;
  * Created by mac on 08.05.16.
  */
 public class ApplicationManager {
+   WebDriver wd;
 
-   FirefoxDriver wd;
-
+   private SessionHelper sessionHelper;
    private  NavigationHelper navigationHelper;
    private GroupHelper groupHelper;
-   private SessionHelper sessionHelper;
    private ContactHelper contactHelper;
+   private String browser;
+
+   public ApplicationManager(String browser) {
+      this.browser = browser;
+   }
 
 
    public void init() {
-      wd = new FirefoxDriver();
+      if (browser == BrowserType.FIREFOX) {
+         wd = new FirefoxDriver();
+      } else if (browser == BrowserType.CHROME) {
+         wd = new ChromeDriver();
+      } else if (browser == BrowserType.IE) {
+         wd = new InternetExplorerDriver();
+      }
       wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
       wd.get("http://localhost/addressbook/group.php");
       groupHelper = new GroupHelper(wd);
