75475d99cfd8cf83f9fba561a7c52f631e3f961a
icanon <icanon@yandex.ru>
2016-05-22 13:41:26 +0400

    добавили проверку до и после в тесты груп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -7,9 +8,14 @@ public class GroupCreationTests extends TestBase {
 
    @Test
    public void testGroupCreation() {
-
       app.getNavigationHelper().gotoGroupPage();
+      int before = app.getGroupHelper().getGroupCount();
       app.getGroupHelper().createGroup(new GroupData("testG1", "testG2", "testG3"));
+      int after = app.getGroupHelper().getGroupCount();
+      Assert.assertEquals(after , before + 1);
+
+
+
 
    }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -15,11 +16,14 @@ public class GroupModificationTests extends TestBase {
       if (! app.getGroupHelper().isThereAGroup()){
          app.getGroupHelper().createGroup(new GroupData("testG1111", "testG2", null));
       }
+      int before = app.getGroupHelper().getGroupCount();
       app.getGroupHelper().selectGroup();
       app.getGroupHelper().initGroupModification();
       app.getGroupHelper().fillGroupForm(new GroupData("testGGGG222", "testGGGG2", "testGGGGG3"));
       app.getGroupHelper().submitGroupModification();
       app.getGroupHelper().returnToGroupPage();
+      int after = app.getGroupHelper().getGroupCount();
+      Assert.assertEquals(after, before);
    }
 
 
