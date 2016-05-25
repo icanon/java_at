a407f5ba06b15e564e6e16fbf8407d447397771f
Ivan Sokolov <i.sokolov@cityads.com>
2016-05-25 19:00:57 +0300

    create comp

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -24,7 +25,10 @@ public class GroupCreationTests extends TestBase {
             max = g.getId();
          }
       }
-      group.setId(max);
+
+      int max1 = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+
+      group.setId(max1);
       before.add(group);
       Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
