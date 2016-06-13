0a82c7400fde0649972b561f86a110d2b06e4d2c
icanon <icanon@yandex.ru>
2016-06-13 20:47:53 +0400

    fix test ho

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -5,17 +5,28 @@ import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.awt.*;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerator {
 
-   public static void main(String[] args){
+   public static void main(String[] args) throws IOException {
       int count = Integer.parseInt(args[0]);
       File file = new File(args[1]);
 
       List<GroupData> groups = generateGroups(count);
       save(groups, file);
+   }
+
+   private static void save(List<GroupData> groups, File file) throws IOException {
+      Writer writer = new FileWriter(file);
+      for(GroupData group : groups) {
+         writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+      }
+      writer.close();
    }
 
    private static List<GroupData> generateGroups(int count){
